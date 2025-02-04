b618a6794acd4a1926d0ffb409ba49ee94ffd69c
Adrian-Mas <adrian.moure.asorey@gmail.com>
2025-02-04 19:18:34 +0000

    FIN

--- src/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask, request, render_template
-from pickle import load 
-
-app = Flask(__name__)
-
-model = load(open(/workspaces/Proyecto_arbol_decision/src/decision_tree_clasiffier_diabetes), "rb")
-
-
